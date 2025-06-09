721f492db5941822ff51646760e7a8cecb26e878
Huron <charlie.walls97@gmail.com>
2025-06-09 15:32:50 -0500

    Creacion del servidor con Ts

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+class Server {
+  app = express();
+  port = 3000;
+  constructor() {
+    this.initializeRoutes();
+  }
+  private initializeRoutes() {
+    this.app.get("/", (req, res) => {
+      res.send("Hello, World!");
+    });
+  }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on http://localhost:${this.port}`);
+    });
+  }
+}
+
+const server = new Server();
+server.listen();
